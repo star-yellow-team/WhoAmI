dff0c59ee9f5a99fe66a336749f3fcb014860f0f
Pavel Stefan <pavel_stefan_96@yahoo.ro>
2014-09-27 00:23:47 +0300

    player-ul moare daca atinge focul :smile:

--- WhoAmIProjectFiles/Assets/Scripts/Fire.cs
+++ WhoAmIProjectFiles/Assets/Scripts/Fire.cs
@@ -13,7 +13,7 @@ public class Fire : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
 	{
-	Debug.Log("Here");
+
 	}
 
 
---
+++ WhoAmIProjectFiles/Assets/Scripts/FireCollision.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//Debug.Log("foc atins");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnTriggerEnter2D(Collider2D target){
+		Debug.Log ("atins");
+		if (target.gameObject.tag == "Player")
+			Destroy (target.gameObject);
+
+	}
+}
